c5e1b803c324f53f18856bd9415faf36de9b2c45
Maghish <mail4mithran@gmail.com>
2024-11-11 20:56:57 +0530

    Added prettier and eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off",
+    "no-unused-vars": "off",
+    "no-useless-escape": "error",
+    quotes: ["error", "double"],
+    semi: "off",
+  },
+};
