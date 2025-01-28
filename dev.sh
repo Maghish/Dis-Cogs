78136e7a3e152dd9f747191a6f2717e7fb650c67
Maghish <mail4mithran@gmail.com>
2025-01-28 11:57:19 +0530

    fix: update main entry point to src directory and bump version to 0.3.0-beta.1

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf dist && echo "[CLEAR] Clear build cache" && npx tsc && echo "[BUILD] Application build complete" && echo "[START] Starting the application" && node dist/main.js
+rm -rf dist && echo "[CLEAR] Clear build cache" && npx tsc && echo "[BUILD] Application build complete" && echo "[START] Starting the application" && node dist/src/main.js
